e17ab62ce2881276a8ab4433a40c8cad2907aa7f
Justin Tadlock <justin.b.tadlock@gmail.com>
2019-03-28 15:53:21 -0700

    Creating Dev area

---
+++ dev/js/my-library.js
@@ -0,0 +1,34 @@
+$(document).ready( function() {
+
+})
+$(function() {
+
+    loadViews();
+
+    var today = getToday();
+
+    $("time").text(today);
+    $("time").attr({datetime: today});
+})
+
+function loadViews()
+{
+    loadView("about");
+    loadView("interests");
+    loadView("skills");
+    loadView("portfolio");
+    loadView("resume");
+}
+
+function loadView(viewName) {
+    var fileName = "./sections/" + viewName + ".html";
+
+    $("#" + viewName).load(fileName);
+}
+
+function getToday() {
+    var currentDate = new Date($.now());
+    currentDate = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear();
+
+    return currentDate;
+}
