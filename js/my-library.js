297a86a8fd6dcf236b837b7b94354b751cbfd168
Justin Tadlock <justin.b.tadlock@gmail.com>
2019-03-28 16:01:19 -0700

    Changes
    - Moved dev branch to be easier to work with in the folder structur
    - Moved the head to a separate include file

--- js/my-library.js
+++ js/my-library.js
@@ -1,30 +1,10 @@
-$(document).ready( function() {
-
-})
 $(function() {
-
-    loadViews();
 
     var today = getToday();
 
     $("time").text(today);
     $("time").attr({datetime: today});
 })
-
-function loadViews()
-{
-    loadView("about");
-    loadView("interests");
-    loadView("skills");
-    loadView("portfolio");
-    loadView("resume");
-}
-
-function loadView(viewName) {
-    var fileName = "./sections/" + viewName + ".html";
-
-    $("#" + viewName).load(fileName);
-}
 
 function getToday() {
     var currentDate = new Date($.now());
