117af90a00a3a41a17275de95019cba1b6031e0f
JustinTadlock <justin.b.tadlock@gmail.com>
2019-03-27 17:33:11 -0700

    Implement Views Functionality
    - Added views functionality using jQuery to clean up the index file and make the sections more maintainable

--- js/my-library.js
+++ js/my-library.js
@@ -1,9 +1,27 @@
 $(function() {
+
+    loadViews();
+
     var today = getToday();
 
     $("time").text(today);
     $("time").attr({datetime: today});
 })
+
+function loadViews()
+{
+    loadView("about");
+    loadView("interests");
+    loadView("skills");
+    loadView("portfolio");
+    loadView("resume");
+}
+
+function loadView(viewName) {
+    var fileName = "./sections/" + viewName + ".html";
+
+    $("#" + viewName).load( fileName );
+}
 
 function getToday() {
     var currentDate = new Date($.now());
