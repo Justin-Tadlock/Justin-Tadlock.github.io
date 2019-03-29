43bbf0b6f6c34bef8030dc44d783d0d8f8e98437
JustinTadlock <justin.b.tadlock@gmail.com>
2019-03-28 19:12:30 -0700

    Reverting to fix site

--- js/my-library.js
+++ js/my-library.js
@@ -6,6 +6,24 @@ $(function() {
     $("time").attr({datetime: today});
 })
 
+<<<<<<< HEAD
+=======
+function loadViews()
+{
+    loadView("about");
+    loadView("interests");
+    loadView("skills");
+    loadView("portfolio");
+    loadView("resume");
+}
+
+function loadView(viewName) {
+    var fileName = "./sections/" + viewName + ".html";
+
+    $("#" + viewName).load( fileName );
+}
+
+>>>>>>> parent of 81f7042... Fixing resume viewing issues
 function getToday() {
     var currentDate = new Date($.now());
     currentDate = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear();
